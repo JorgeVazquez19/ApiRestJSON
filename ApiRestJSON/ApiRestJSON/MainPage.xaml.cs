ef34b9b792c512e99d282af65551e0afe6a61cb1
unknown <usuario@GFI10188.gfi.es>
2019-03-14 13:51:39 +0100

    Button Add implemented

--- ApiRestJSON/ApiRestJSON/MainPage.xaml.cs
+++ ApiRestJSON/ApiRestJSON/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -29,5 +30,28 @@ namespace ApiRestJSON
 
             listaCursos.ItemsSource = modelo.Cursos;
         }
+
+        private async void Agregar_Click(object sender, EventArgs args)
+        {
+            Curso curso = new Curso();
+            curso.Nombre = "Xamarin";
+            curso.Duracion = 40;
+
+            HttpClient cliente = new HttpClient();
+
+            string url = "http://tgconsulting.online/tg-rest/servicio.php/cursos";
+
+            String jsonCurso = JsonConvert.SerializeObject(curso);
+            var resultado = await cliente.PostAsync(url, new StringContent(jsonCurso));
+            var json = resultado.Content.ReadAsStringAsync().Result;
+
+            await DisplayAlert("Resultado", json, "Ok");
+        }
+
+        private void Eliminar_Click(object sender, EventArgs args)
+        {
+
+        }
+
     }
 }
