9402faf673f5504a67daa4c562fae288d2576686
unknown <usuario@GFI10188.gfi.es>
2019-03-14 12:20:46 +0100

    First Commit

---
+++ ApiRestJSON/ApiRestJSON/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ApiRestJSON
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LlenarCursos();
+        }
+
+        private async void LlenarCursos()
+        {
+            HttpClient cliente = new HttpClient();
+
+            string url = "http://tgconsulting.online/tg-rest/servicio.php/cursos";
+
+            var resultado = await cliente.GetAsync(url);
+            var json = resultado.Content.ReadAsStringAsync().Result;
+
+            CursosModel modelo = CursosModel.FromJson(json);
+
+            listaCursos.ItemsSource = modelo.Cursos;
+        }
+    }
+}
